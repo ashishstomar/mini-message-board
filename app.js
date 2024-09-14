969552a6c940b7bde46a287ff072b95c9c86f65c
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-09-15 01:52:53 +0530

    feat:route handling for GET and POST requests

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const path = require("path");
+const router = require("./routes/routes");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.get("/", (req, res) => {
-  return res.render("home");
-});
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/new", (req, res) => {});
+app.get("/", router);
+
+app.post("/new", router);
 
 const PORT = 3000;
 app.listen(PORT, () => `Server started at PORT ${PORT}`);
